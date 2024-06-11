39225141b8464f9b91dd9d001564584b6bb5ebe6
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-06-11 13:18:25 +0200

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -49,6 +49,66 @@ final class Plugin {
 	 * @return void
 	 */
 	public function setup() {
+		\add_action( 'http_api_curl', [ $this, 'http_api_curl' ], 10, 2 );
+	}
 
+	/**
+	 * HTTP API cURL.
+	 * 
+	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array    $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	public function http_api_curl( $handle, $parsed_args ) {
+		$this->set_ssl_certificate_blob_option_if_needed( $handle, $parsed_args );
+		$this->set_ssl_key_blob_option_if_needed( $handle, $parsed_args );
+		$this->set_ssl_key_password_option_if_needed( $handle, $parsed_args );
+	}
+
+	/**
+	 * Set SSL certificate password option if needed.
+	 * 
+	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array    $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	private function set_ssl_certificate_blob_option_if_needed( $handle, $parsed_args ) {
+		/**
+		* Curl blob option.
+		*
+		* @link https://github.com/php/php-src/blob/php-8.1.0/ext/curl/interface.c#L2935-L2955
+		*/
+		if ( \array_key_exists( 'ssl_certificate_blob', $parsed_args ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
+			\curl_setopt( $handle, \CURLOPT_SSLCERT_BLOB, $parsed_args['ssl_certificate_blob'] );
+		}
+	}
+
+	/**
+	 * Set SSL key blob option if needed.
+	 * 
+	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array    $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	private function set_ssl_key_blob_option_if_needed( $handle, $parsed_args ) {
+		if ( \array_key_exists( 'ssl_key_blob', $parsed_args ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
+			\curl_setopt( $handle, \CURLOPT_SSLKEY_BLOB, $parsed_args['ssl_key_blob'] );
+		}
+	}
+
+	/**
+	 * Set SSL key password option if needed.
+	 * 
+	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array    $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	private function set_ssl_key_password_option_if_needed( $handle, $parsed_args ) {
+		if ( \array_key_exists( 'ssl_key_password', $parsed_args ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
+			\curl_setopt( $handle, \CURLOPT_SSLKEYPASSWD, $parsed_args['ssl_key_password'] );
+		}
 	}
 }
