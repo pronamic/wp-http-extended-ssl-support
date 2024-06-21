3ec2252d51c2ddcfe5f764a668c12417d612dc06
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-06-21 11:48:52 +0200

    wip

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 This library extends the WordPress HTTP API with additional SSL options. It provides the option to pass the cURL options `CURLOPT_SSLCERT`, `CURLOPT_SSLKEY` and `CURLOPT_SSLKEYPASSWD` to requests.
 
+## Usage
+
+```php
+\wp_remote_get(
+	$url,
+	[
+		'ssl_certificate'  => \ABSPATH . '/../private/your-certificate.pem',
+		'ssl_key'          => \ABSPATH . '/../private/your-key.pem',
+		'ssl_key_password' => ' your-password', 
+	]
+);
+```
+
+```php
+\wp_remote_get(
+	$url,
+	[
+		'ssl_certificate_blob'   => '-----BEGIN CERTIFICATE-----', // Must be a full SSL certificate string.
+		'ssl_key_blob'           => '-----BEGIN ENCRYPTED PRIVATE KEY-----', // Must be a full SSL key string.
+		'ssl_key_password' => ' your-password', 
+	]
+);
+```
+
 ## Links
 
 - https://github.com/WordPress/Requests/issues/377
--- src/Plugin.php
+++ src/Plugin.php
@@ -10,6 +10,8 @@
 
 namespace Pronamic\WpExtendedSslSupport;
 
+use CurlHandle;
+
 /**
  * Plugin class
  */
@@ -24,7 +26,7 @@ final class Plugin {
 	/**
 	 * Bootstrap.
 	 * 
-	 * @retun void
+	 * @return void
 	 */
 	public static function bootstrap() {
 		self::instance()->setup();
@@ -59,22 +61,25 @@ final class Plugin {
 	/**
 	 * HTTP API cURL.
 	 * 
-	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
-	 * @param array    $parsed_args The HTTP request arguments.
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
 	 * @return void
 	 */
 	public function http_api_curl( $handle, $parsed_args ) {
 		$this->set_ssl_certificate_blob_option_if_needed( $handle, $parsed_args );
+		$this->set_ssl_certificate_option_if_needed( $handle, $parsed_args );
 		$this->set_ssl_key_blob_option_if_needed( $handle, $parsed_args );
+		$this->set_ssl_key_option_if_needed( $handle, $parsed_args );
 		$this->set_ssl_key_password_option_if_needed( $handle, $parsed_args );
 	}
 
 	/**
-	 * Set SSL certificate password option if needed.
+	 * Set SSL certificate BLOB option if needed.
 	 * 
-	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
-	 * @param array    $parsed_args The HTTP request arguments.
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
 	 * @return void
+	 * @throws \Exception If the BLOB options is not supported and falling back to a temporary file fails.
 	 */
 	private function set_ssl_certificate_blob_option_if_needed( $handle, $parsed_args ) {
 		/**
@@ -83,30 +88,126 @@ final class Plugin {
 		* @link https://github.com/php/php-src/blob/php-8.1.0/ext/curl/interface.c#L2935-L2955
 		*/
 		if ( \array_key_exists( 'ssl_certificate_blob', $parsed_args ) ) {
+			try {
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
+				\curl_setopt( $handle, \CURLOPT_SSLCERT_BLOB, $parsed_args['ssl_certificate_blob'] );
+			} catch ( \ValueError $error ) {
+				/**
+				 * Not all TLS backends support BLOB, therefore we fall back on a SSL certificate file.
+				 * 
+				 * @link https://curl.se/libcurl/c/tls-options.html
+				 * @link https://github.com/pronamic/wp-http-extended-ssl-support/issues/1
+				 */
+
+				// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_tempnam -- Recommended function `get_temp_dir()` is used.
+				$file = \tempnam( \get_temp_dir(), 'pronamic_curl_sslcert_blob' );
+
+				if ( false === $file ) {
+					$exception = new \Exception( 'Failed to create a temporary file for SSL certificate BLOB.', 0, $error );
+
+					throw $exception;
+				}
+
+				// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_file_put_contents -- Allowed when using temporary file.
+				$result = \file_put_contents( $file, $parsed_args['ssl_certificate_blob'] );
+
+				if ( false === $result ) {
+					$exception = new \Exception( 'Failed to write SSL certificate BLOB to temporary file.', 0, $error );
+
+					throw $exception;
+				}
+
+				$parsed_args['ssl_certificate'] = $file;
+
+				$this->set_ssl_certificate_option_if_needed( $handle, $parsed_args );
+			}
+		}
+	}
+
+	/**
+	 * Set SSL certificate option if needed.
+	 * 
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	private function set_ssl_certificate_option_if_needed( $handle, $parsed_args ) {
+		/**
+		* Curl blob option.
+		*
+		* @link https://github.com/php/php-src/blob/php-8.1.0/ext/curl/interface.c#L2935-L2955
+		*/
+		if ( \array_key_exists( 'ssl_certificate', $parsed_args ) ) {
 			// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
-			\curl_setopt( $handle, \CURLOPT_SSLCERT_BLOB, $parsed_args['ssl_certificate_blob'] );
+			\curl_setopt( $handle, \CURLOPT_SSLCERT, $parsed_args['ssl_certificate'] );
 		}
 	}
 
 	/**
 	 * Set SSL key blob option if needed.
 	 * 
-	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
-	 * @param array    $parsed_args The HTTP request arguments.
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
 	 * @return void
+	 * @throws \Exception If the BLOB options is not supported and falling back to a temporary file fails.
 	 */
 	private function set_ssl_key_blob_option_if_needed( $handle, $parsed_args ) {
 		if ( \array_key_exists( 'ssl_key_blob', $parsed_args ) ) {
+			try {
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
+				\curl_setopt( $handle, \CURLOPT_SSLKEY_BLOB, $parsed_args['ssl_key_blob'] );
+			} catch ( \ValueError $error ) {
+				/**
+				 * Not all TLS backends support BLOB, therefore we fall back on a SSL key file.
+				 * 
+				 * @link https://curl.se/libcurl/c/tls-options.html
+				 * @link https://github.com/pronamic/wp-http-extended-ssl-support/issues/1
+				 */
+
+				// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_tempnam -- Recommended function `get_temp_dir()` is used.
+				$file = \tempnam( \get_temp_dir(), 'pronamic_curl_sslkey_blob' );
+
+				if ( false === $file ) {
+					$exception = new \Exception( 'Failed to create temporary file for SSL key BLOB.', 0, $error );
+
+					throw $exception;
+				}
+
+				// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_file_put_contents -- Allowed when using temporary file. 
+				$result = \file_put_contents( $file, $parsed_args['ssl_certificate_blob'] );
+
+				if ( false === $result ) {
+					$exception = new \Exception( 'Failed to write SSL key BLOB to temporary file.', 0, $error );
+
+					throw $exception;
+				}
+
+				$parsed_args['ssl_key'] = $file;
+
+				$this->set_ssl_key_option_if_needed( $handle, $parsed_args );
+			}
+		}
+	}
+
+	/**
+	 * Set SSL key option if needed.
+	 * 
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
+	 * @return void
+	 */
+	private function set_ssl_key_option_if_needed( $handle, $parsed_args ) {
+		if ( \array_key_exists( 'ssl_key', $parsed_args ) ) {
 			// phpcs:ignore WordPress.WP.AlternativeFunctions.curl_curl_setopt -- WordPress requests library does not support this yet.
-			\curl_setopt( $handle, \CURLOPT_SSLKEY_BLOB, $parsed_args['ssl_key_blob'] );
+			\curl_setopt( $handle, \CURLOPT_SSLKEY, $parsed_args['ssl_key'] );
 		}
 	}
 
 	/**
 	 * Set SSL key password option if needed.
 	 * 
-	 * @param resource $handle      The cURL handle returned by curl_init() (passed by reference).
-	 * @param array    $parsed_args The HTTP request arguments.
+	 * @param CurlHandle            $handle      The cURL handle returned by curl_init() (passed by reference).
+	 * @param array<string, string> $parsed_args The HTTP request arguments.
 	 * @return void
 	 */
 	private function set_ssl_key_password_option_if_needed( $handle, $parsed_args ) {
